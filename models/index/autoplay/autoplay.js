350f0745508320d834bd6cba7182c782dcbae36e
WGDG <1820717381@qq.com>
2018-12-09 18:35:46 +0800

    缓存机制

--- models/index/autoplay/autoplay.js
+++ models/index/autoplay/autoplay.js
@@ -2,20 +2,38 @@ import { HTTP } from '../../../utils/http.js'
 
 class AutoPlay extends HTTP {
   getDataNext(index,callback) {
-    this.request({
-      url: '/classic/' + index + '/next',
-      success(res) {
-        callback(res)
-      }
-    })
+    let _getkey = `classc-${index + 1}`
+    let classic = wx.getStorageSync(_getkey)
+    if(!classic) {
+      this.request({
+        url: '/classic/' + index + '/next',
+        success(res) {
+          console.log('请求了接口')
+          wx.setStorageSync(_getkey, res)
+          callback(res)
+        }
+      }) 
+    }else{
+      callback(classic)
+    }
+    
   }
   getDataPrev(index, callback) {
-    this.request({
-      url: '/classic/'+ index +'/previous',
-      success(res) {
-        callback(res)
-      }
-    })
+    let _getkey = `classc-${index - 1}`
+    let classic = wx.getStorageSync(_getkey)
+    if (!classic) {
+      this.request({
+        url: '/classic/' + index + '/previous',
+        success(res) {
+          wx.setStorageSync(_getkey, res)
+          console.log('下一页请求了接口')
+          callback(res)
+        }
+      })
+    } else {
+      callback(classic)
+    }
+  
   }
 }
 export { AutoPlay }
--- models/index/index.js
+++ models/index/index.js
@@ -2,9 +2,12 @@ import { HTTP } from '../../utils/http.js'
 
 class IndexModel extends HTTP {
   getDataLatest(callback) {
+   
     this.request({
       url: 'classic/latest',
       success(res) {
+        let _getkey = `classc-${res.index}`
+        wx.setStorageSync(_getkey, res)
         callback(res)
       }
     })
