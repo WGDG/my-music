ab5bbf0b925c4eedf6c047768c8d239fb4279b36
WG <1340093882@qq.com>
2018-12-12 14:01:48 +0800

    插槽

--- components/bookesdetail/pingjia/pingjia.js
+++ components/bookesdetail/pingjia/pingjia.js
@@ -1,5 +1,11 @@
 // components/bookesdetail/pingjia/pingjia.js
 Component({
+  externalClasses:["first","second"],
+
+  // slot
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
   /**
    * 组件的属性列表
    */
